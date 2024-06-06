0c814d991141eba136806b7943f40c56a64c6572
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-06-06 12:55:27 +0800

    update vercel.json

--- src/index.js
+++ src/index.js
@@ -8,23 +8,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const client_1 = require("@prisma/client");
-const bcryptjs_1 = __importDefault(require("bcryptjs"));
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = require("express");
+var client_1 = require("@prisma/client");
+var bcryptjs_1 = require("bcryptjs");
+var jsonwebtoken_1 = require("jsonwebtoken");
+var swagger_jsdoc_1 = require("swagger-jsdoc");
+var swagger_ui_express_1 = require("swagger-ui-express");
+var dotenv_1 = require("dotenv");
 // 加載 .env 文件中的環境變數
 dotenv_1.default.config();
-const prisma = new client_1.PrismaClient();
-const app = (0, express_1.default)();
+var prisma = new client_1.PrismaClient();
+var app = (0, express_1.default)();
 app.use(express_1.default.json());
-const swaggerOptions = {
+var swaggerOptions = {
     swaggerDefinition: {
         openapi: "3.0.0",
         info: {
@@ -35,31 +59,61 @@ const swaggerOptions = {
     },
     apis: ["./src/index.ts"],
 };
-const swaggerDocs = (0, swagger_jsdoc_1.default)(swaggerOptions);
+var swaggerDocs = (0, swagger_jsdoc_1.default)(swaggerOptions);
 app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocs));
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+var JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 // User routes
-app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password, name } = req.body;
-    const hashedPassword = yield bcryptjs_1.default.hash(password, 10);
-    const user = yield prisma.user.create({
-        data: { email, password: hashedPassword, name },
+app.post("/register", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, email, password, name, hashedPassword, user;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = req.body, email = _a.email, password = _a.password, name = _a.name;
+                return [4 /*yield*/, bcryptjs_1.default.hash(password, 10)];
+            case 1:
+                hashedPassword = _b.sent();
+                return [4 /*yield*/, prisma.user.create({
+                        data: { email: email, password: hashedPassword, name: name },
+                    })];
+            case 2:
+                user = _b.sent();
+                res.json(user);
+                return [2 /*return*/];
+        }
     });
-    res.json(user);
-}));
-app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    const user = yield prisma.user.findUnique({ where: { email } });
-    if (!user || !(yield bcryptjs_1.default.compare(password, user.password))) {
-        return res.status(401).send("Invalid email or password");
-    }
-    const token = jsonwebtoken_1.default.sign({ userId: user.id }, JWT_SECRET);
-    res.json({ token });
-}));
-app.post("/forgot-password", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // Implement forgot password logic here
-    res.send("Forgot password endpoint");
-}));
+}); });
+app.post("/login", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, email, password, user, _b, token;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                _a = req.body, email = _a.email, password = _a.password;
+                return [4 /*yield*/, prisma.user.findUnique({ where: { email: email } })];
+            case 1:
+                user = _c.sent();
+                _b = !user;
+                if (_b) return [3 /*break*/, 3];
+                return [4 /*yield*/, bcryptjs_1.default.compare(password, user.password)];
+            case 2:
+                _b = !(_c.sent());
+                _c.label = 3;
+            case 3:
+                if (_b) {
+                    return [2 /*return*/, res.status(401).send("Invalid email or password")];
+                }
+                token = jsonwebtoken_1.default.sign({ userId: user.id }, JWT_SECRET);
+                res.json({ token: token });
+                return [2 /*return*/];
+        }
+    });
+}); });
+app.post("/forgot-password", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        // Implement forgot password logic here
+        res.send("Forgot password endpoint");
+        return [2 /*return*/];
+    });
+}); });
 // Company routes
 // app.get("/companies", async (req, res) => {
 //   const companies = await prisma.company.findMany();
@@ -135,7 +189,7 @@ app.post("/forgot-password", (req, res) => __awaiter(void 0, void 0, void 0, fun
  *       200:
  *         description: List of companies
  */
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+var PORT = process.env.PORT || 4000;
+app.listen(PORT, function () {
+    console.log("Server is running on port ".concat(PORT));
 });
